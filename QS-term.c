ecfb402a64c16c3d0916d603d345a7d3cb3a47f8
CowerDawn <bigdickpopboba@gmail.com>
2024-12-04 12:19:40 +0000

    Update QS-term.c

--- QS-term.c
+++ QS-term.c
@@ -22,8 +22,8 @@ static void set_theme(VteTerminal *terminal, const char *theme) {
         GdkRGBA bg = {0.15, 0.15, 0.15, 1.0};
         vte_terminal_set_colors(terminal, &fg, &bg, NULL, 0);
     } else if (strcmp(theme, THEME_PINK) == 0) {
-        GdkRGBA fg = {1.0, 0.5, 0.75, 1.0};
-        GdkRGBA bg = {1.0, 0.85, 0.9, 1.0};
+        GdkRGBA fg = {0.0, 0.0, 0.0, 1.0}; 
+        GdkRGBA bg = {0.8, 0.65, 0.7, 1.0}; 
         vte_terminal_set_colors(terminal, &fg, &bg, NULL, 0);
     }
 }
@@ -38,6 +38,12 @@ static void on_paste_clipboard(GtkWidget *widget, gpointer data) {
     vte_terminal_paste_clipboard(terminal);
 }
 
+static void on_set_title(GtkWidget *widget, gpointer data) {
+    GtkWidget *window = GTK_WIDGET(data);
+    const gchar *title = gtk_menu_item_get_label(GTK_MENU_ITEM(widget));
+    gtk_window_set_title(GTK_WINDOW(window), title);
+}
+
 static gboolean on_key_press_event(GtkWidget *widget, GdkEventKey *event, gpointer data);
 
 static void on_app_activate(GtkApplication *app, gpointer user_data) {
@@ -46,10 +52,17 @@ static void on_app_activate(GtkApplication *app, gpointer user_data) {
     GtkWidget *menu;
     GtkWidget *copy_item;
     GtkWidget *paste_item;
+    GtkWidget *title_item;
 
     window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window), "QS-term");
     gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
+
+    GdkPixbuf *icon = gdk_pixbuf_new_from_file("icon.png", NULL);
+    if (icon) {
+        gtk_window_set_icon(GTK_WINDOW(window), icon);
+        g_object_unref(icon);
+    }
 
     terminal = vte_terminal_new();
     vte_terminal_spawn_async(VTE_TERMINAL(terminal),
@@ -72,12 +85,15 @@ static void on_app_activate(GtkApplication *app, gpointer user_data) {
     menu = gtk_menu_new();
     copy_item = gtk_menu_item_new_with_label("Copy");
     paste_item = gtk_menu_item_new_with_label("Paste");
+    title_item = gtk_menu_item_new_with_label("Set Title");
     gtk_menu_shell_append(GTK_MENU_SHELL(menu), copy_item);
     gtk_menu_shell_append(GTK_MENU_SHELL(menu), paste_item);
+    gtk_menu_shell_append(GTK_MENU_SHELL(menu), title_item);
     gtk_widget_show_all(menu);
 
     g_signal_connect(copy_item, "activate", G_CALLBACK(on_copy_clipboard), terminal);
     g_signal_connect(paste_item, "activate", G_CALLBACK(on_paste_clipboard), terminal);
+    g_signal_connect(title_item, "activate", G_CALLBACK(on_set_title), window);
 
     g_signal_connect(terminal, "button-press-event", G_CALLBACK(gtk_menu_popup_at_widget), menu);
 
@@ -110,6 +126,9 @@ static gboolean on_key_press_event(GtkWidget *widget, GdkEventKey *event, gpoint
         return TRUE;
     } else if (event->state & GDK_CONTROL_MASK && event->keyval == GDK_KEY_q) {
         vte_terminal_reset(terminal, TRUE, TRUE);
+        return TRUE;
+    } else if (event->state & GDK_SHIFT_MASK && event->keyval == GDK_KEY_Home) {
+        vte_terminal_copy_clipboard_format(terminal, VTE_FORMAT_TEXT);
         return TRUE;
     }
 
